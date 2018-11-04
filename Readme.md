b1a08869439f85e759e929604fe969c786775151
Rinat <rinat.babichev@gmail.com>
2018-11-04 21:22:37 +0300

    DE

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# DE assignment on numerical methods
+Babichev Rinat, BS17 - 08
--- main.py
+++ main.py
@@ -44,11 +44,13 @@ def solution(xs):
         ys += [exact_solution(xi)]
     return ys
 
+
 """f - callable function of x and y such that y'=f(x,y),
     xs - array of x-values
     y0 - given ivp value for xs[0]
     h - step
 """
+
 
 @calculate_error
 def euler(f, xs, y0, h):
@@ -79,6 +81,34 @@ def runge_khutta(f, xs, y0, h):
         ys += [ys[i - 1] + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)]
     return ys
 
+
+def print_errors(xs, error_euler, error_improved, error_runge):
+    plt.plot(xs, error_euler, 'b', label=euler_)
+    plt.plot(xs, error_improved, 'y', label=improved_)
+    plt.plot(xs, error_runge, 'g', label=runge_)
+    plt.legend()  # show title for each graph
+    plt.show()
+
+
+def global_error(x0, x1, y0):
+    error_euler = []
+    error_improved_euler = []
+    error_runge_kutta = []
+    N = range(10, 2000)
+    for n in N:
+        h = (x1 - x0) / n
+        xs = numpy.arange(x0, x1 + h, h)
+        ysol = solution(xs)
+        tup = (func, xs, y0, h, ysol)
+        _, error1 = euler(*tup)
+        _, error2 = improved_euler(*tup)
+        _, error3 = runge_khutta(*tup)
+        error_euler.append(max(error1))
+        error_improved_euler.append(max(error2))
+        error_runge_kutta.append(max(error3))
+    print_errors(N, error_euler, error_improved_euler, error_runge_kutta)
+
+
 if __name__ == '__main__':
     euler_, improved_, runge_, exact_ = 'Euler', 'Improved Euler', 'Runge Kutta', 'Exact solution'
 
@@ -91,26 +121,22 @@ if __name__ == '__main__':
     ysol = solution(xs)
     tup = (func, xs, y0, h, ysol)
     # draw euler
-    y, error1 = list(euler(*tup))
-    euler_plot = plt.plot(xs, y, 'b', label=euler_)
+    y, error1 = euler(*tup)
+    plt.plot(xs, y, 'b', label=euler_)
     # draw exact solution
-    exact_plot = plt.plot(xs, ysol, 'r', label=exact_)
+    plt.plot(xs, ysol, 'r', label=exact_)
     # draw improved euler
-    y, error2 = list(improved_euler(*tup))
-    impr_plot = plt.plot(xs, y, 'y', label=improved_)
+    y, error2 = improved_euler(*tup)
+    plt.plot(xs, y, 'y', label=improved_)
     # draw runge-khutta
-    y, error3 = list(runge_khutta(*tup))
-    runge_plot = plt.plot(xs, y, 'g', label=runge_)
-
+    y, error3 = runge_khutta(*tup)
+    plt.plot(xs, y, 'g', label=runge_)
     plt.legend()
     plt.show()
     # plot errors
-    plt.plot(xs, error1, 'b', label=euler_)
-    plt.plot(xs, error2, 'y', label=improved_)
-    plt.plot(xs, error3, 'g', label=runge_)
-    plt.legend()
-    plt.show()
+    print_errors(xs, error1, error2, error3)
     # plot runge-khutta error with more precision separately
     plt.plot(xs, error3, 'g', label=runge_)
     plt.legend()
     plt.show()
+    global_error(x0, x1, y0)
